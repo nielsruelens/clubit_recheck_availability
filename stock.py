ec8464415120c49e1474956d91dbfc370ee1da5a
Niels Ruelens <niels.ruelens@clubit.be>
2014-08-01 11:14:23 +0200

    added prioritise functionality, bitches

--- stock.py
+++ stock.py
@@ -28,3 +28,28 @@ class stock_picking_out(osv.Model):
         for pick in self.browse(cr, uid, ids):
             wf_service.trg_validate(uid, 'stock.picking', pick.id, 'button_confirm_assigned_back', cr)
         return True
+
+
+
+    def action_prioritize(self, cr, uid, ids, *args):
+        """ Prioritize pickings
+        @return: True
+        """
+        if len(ids) > 1:
+            raise osv.except_osv(_('Not allowed!'), _('You may only prioritize 1 delivery at a time!'))
+
+        move_db = self.pool.get('stock.move')
+        pick = self.browse(cr, uid, ids)[0]
+        products = [m.product_id.id for m in pick.move_lines]
+        moves = move_db.search(cr, uid, [('product_id','in',products),('state', '=', 'assigned')])
+        moves = move_db.browse(cr, uid, moves)
+        self.action_back_to_confirmed(cr, uid, list(set([m.picking_id.id for m in moves])))
+        self.action_assign(cr, uid, ids)
+        return True
+
+    def action_maximize(self, cr, uid, ids, *args):
+        """ Maximize pickings
+        @return: True
+        """
+        return True
+
